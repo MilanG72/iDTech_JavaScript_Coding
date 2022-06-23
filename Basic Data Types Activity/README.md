7472f4c3f139069092a88c1f096574d5a326bc81
Milan Gupta <milanvillain331@gmail.com>
2022-06-23 11:13:27 -0700

    new file

--- Basic Data Types Activity/README.md
+++ Basic Data Types Activity/README.md
@@ -1,1 +1,2 @@
 # Working with Basic Data Types Activity
+In this assignment, I used the basic type variables including strings, arrays, boolean, and integers. I used these elements to create a story.
---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,37 @@
+// TODO: Create 4-String variables to introduce develop your story.
+let greeting = "Hello fellow traveler";
+let name = "Robotnik";
+let title = "Robot Creator";
+let worldName = "Kingdom in the Clouds";
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+let year = 153435;
+let century = 153;
+let age = 7001;
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+let weaponWielded = ["Ancient Staff", "Cyborg Gun", "Mechanical Shield"];
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+let isThisCityFriendly = true;
+
+
+// TODO: Print your story to the console.
+console.log(greeting + " my friends call me " + name + ".");
+console.log("I am the " + title + ".");
+console.log("You are probably wondering what year this strange place is in")
+console.log("and what this place is called....")
+console.log("This place is referred to as the " + worldName + ".")
+console.log("The current year is " + year + ".")
+console.log("The current century is " + century + " and I am 7001 years old" + ".")
+console.log("My birthday was yesterday"+ ".")
+console.log("Anyways, I am an all powerful being" + ".")
+console.log("I wield many weapons including an " + weaponWielded[0] + ", a " + weaponWielded[1] + ", and a " + weaponWielded[2] + ".")
+console.log("Do you have any questions?")
+console.log("Traveler: Is this city friendly?")
+console.log("Robotnik:" + isThisCityFriendly + " you will be welcomed with opened arms" + ".")
+console.log("Traveler: Thank you for the tour" + ".")
+console.log("Robotnik: No problem traveler" + ".")
